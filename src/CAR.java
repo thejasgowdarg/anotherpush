4d221d0a384e3f2175b8af7fd23ba9247eb42acc
Thejas <thejasrg45@gmail.com>
2025-08-03 13:09:55 +0530

    vvv

---
+++ src/CAR.java
@@ -0,0 +1,32 @@
+
+public class CAR {
+	
+	int cost;
+	String model;
+	String color;
+	int milage;
+	
+	public void Carinshowrrom() {
+		
+		System.out.println(model+" is in showroom");
+		
+	}
+public void Cameoutfromshowroom() {
+		
+		System.out.println(model+" is out showroom");
+		
+	}
+
+public void Stratcar() {
+	
+	System.out.println(model+" is in startrd");
+	
+}
+public void Stoppcar() {
+	
+	System.out.println(model+" is in stopped");
+	System.out.println("--------------------");
+	
+}
+
+}
---
+++ src/Demo.java
@@ -0,0 +1,32 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		CAR c1=new  CAR();
+		
+		c1.cost=30000;
+		c1.model="audi";
+		c1.color="red";
+		c1.milage=34;
+		
+		c1.Carinshowrrom();
+		c1.Cameoutfromshowroom();
+		c1.Stratcar();
+		c1.Stoppcar();
+		
+		CAR c2=new CAR();
+		
+		c2.cost=345555;
+		c2.model="bmw";
+		c2.color="green";
+		c2.milage=31;
+		
+		c2.Carinshowrrom();
+		c2.Cameoutfromshowroom();
+		c2.Stratcar();
+		c2.Stoppcar();
+
+	}
+
+}
